c4c5fe0cc0a7d4627150397f041215bee77f1c25
CraftMojo <setiawan.yanto@gmail.com>
2018-12-08 14:55:44 +0700

    Initial commit

---
+++ src/net/craftmojo/programku/PersegiPanjang.java
@@ -0,0 +1,28 @@
+package net.craftmojo.programku;
+
+public class PersegiPanjang {
+    private int p;
+    private int l;
+
+    public int getP() {
+        return p;
+    }
+
+    public void setP(int p) {
+        this.p = p;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+
+    public int luasPersegiPanjang() {
+        //proses
+        return this.p * this.l;
+    }
+
+}
---
+++ src/net/craftmojo/programku/Segitiga.java
@@ -0,0 +1,28 @@
+package net.craftmojo.programku;
+
+public class Segitiga {
+    private int a;
+    private int t;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getT() {
+        return t;
+    }
+
+    public void setT(int t) {
+        this.t = t;
+    }
+
+    public int luasSegitiga() {
+        //proses
+        return (this.a * this.t)/2;
+    }
+
+}
